173b79570f26aba5f5ccfa3805529940040ebbad
Fulvio Coelho <coelho.fulvio@gmail.com>
2021-06-23 23:51:53 -0300

    aula 3

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express';
+import { CreateUserController } from './controllers/CreateUserController';
+import { CreateTagController } from './controllers/CreateTagController'
+import { ensureAdmin } from './middlewares/ensureAdmin';
 
 const routes = Router();
 
-routes.post('/users')
+const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
+
+routes.post('/user', createUserController.handler);
+routes.post('/tag', ensureAdmin, createTagController.handler);
 
 export { routes }
