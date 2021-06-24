8b83cbcf859c4affd1ed0ea72c0d58ad3412ac36
Fulvio Coelho <coelho.fulvio@gmail.com>
2021-06-23 21:21:53 -0300

    aula 1 - 2 arquivos corretos

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import 'reflect-metadata'
+import express from 'express'
+import { routes } from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+const PORT = 3000
+
+app.listen(PORT, () => console.log(`listen in port ${PORT}`));
--- src/test/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from "chai"
-
-describe('teste', () => {
-  it('passes', () => {
-    expect(1).to.be.eq(1)
-  })
-})
