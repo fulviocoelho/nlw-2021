a8acd1ab03090e040389e12267e1b6503f77f6be
Fulvio Coelho <coelho.fulvio@gmail.com>
2021-06-24 23:52:02 -0300

    Aula 4

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,14 +1,16 @@
 import { getCustomRepository } from "typeorm";
 import { UsersRepositories } from '../repositories/UsersRepositories'
+import { hash } from 'bcryptjs'
 
 interface IUserRequest {
   name: string;
   email: string;
   admin?: boolean;
+  password: string;
 }
 
 class CreateUserService {
-  async execute({ name, email, admin }: IUserRequest) {
+  async execute({ name, email, admin = false, password }: IUserRequest) {
     const userRepository = getCustomRepository(UsersRepositories)
     if(!email) {
       throw new Error('Email incorrect')
@@ -19,10 +21,13 @@ class CreateUserService {
     if(userAlreadyExists){
       throw new Error('User Already Exists')
     } 
+
+    const passwordHash = await hash(password, 8);
     const user = userRepository.create({
       name,
       email,
       admin,
+      password: passwordHash
     })
 
     await userRepository.save(user)
